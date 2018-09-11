c0b9c98fe4c75a2b7425d6371e2f3c1f58bf448b
Yukee Han <yukeehan@gmail.com>
2018-09-11 10:00:07 -0400

    change the port

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.put("/image", (req, res)=>{ image.handleImage(req, res, db) });
 app.post("/imageurl", (req, res) =>{image.handleApiCall(req, res)});
 
 const PORT = process.env.PORT;
-app.listen(3000, ()=>{
+app.listen(PORT || 3000, ()=>{
 	console.log('app started');
 });
