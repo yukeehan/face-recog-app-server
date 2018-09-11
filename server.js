8a3fd77cc1f945477e438f2ffb556ab03a94fe70
Yukee Han <yukeehan@gmail.com>
2018-09-11 10:03:24 -0400

    change response

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+app.get('/', (req, res)=> { res.send("it is working")});
 app.post("/login", login.handleLogin(db, bcrypt));
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get("/profile/:id", (req, res) => { profile.handleProfile(req, res, db) });
